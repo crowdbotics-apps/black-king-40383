58b9b191683456811315e0e82ef34b91979681c7
Crowdbotics <engineering@crowdbotics.com>
2023-05-02 00:51:26 +0000

    Update store.

---
+++ store/docusignAPI/api.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+import { DOCUSIGN_APIS_SERVICES_TOKEN } from "react-native-dotenv"
+const docusignAPI = axios.create({
+  baseURL: "https://demo.docusign.net",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${DOCUSIGN_APIS_SERVICES_TOKEN}`
+  }
+})
+function docusignapi_get__restapi_v21_accounts_accountId_envelopes_list(
+  payload
+) {
+  return docusignAPI.get(
+    ` /restapi/v2.1/accounts/${payload.accountId}/envelopes`,
+    { params: { folder_ids: payload.folder_ids } }
+  )
+}
+function docusignapi_get_restapi_v21_accounts_accountId_envelopes_envelopeId_read(
+  payload
+) {
+  return docusignAPI.get(
+    `/restapi/v2.1/accounts/${payload.accountId}/envelopes/${payload.envelopeId}`
+  )
+}
+export const apiService = {
+  docusignapi_get__restapi_v21_accounts_accountId_envelopes_list,
+  docusignapi_get_restapi_v21_accounts_accountId_envelopes_envelopeId_read
+}
